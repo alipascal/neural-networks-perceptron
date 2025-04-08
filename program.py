5e8a965811f98dad3a5a01549554172606498cc5
alipascal <alisaumon.prv@gmail.com>
2025-04-08 20:21:02 +0200

    modif prediction test management

--- program.py
+++ program.py
@@ -39,8 +39,14 @@ if data[classification].dtype != 'int64':
 # Début du progamme
 N = data.shape[0]
 data.iloc[:, :2] = perceptron.normalize(data.iloc[:, :2])
-test = data.iloc[-1]
-data =  data.iloc[:-1]
+
+do_test = None
+if "?" in data.iloc[-1]:
+    do_test = True
+
+if do_test:
+    test = data.iloc[-1]
+    data = data.iloc[:-1]
 
 p = Perceptron(epochs=1000, nu=0.01)
 data = numpy.array(data)
@@ -49,9 +55,10 @@ t = time()
 p.fit(data[:, :2],data[:, 2])
 print(f"Fin entrainement ({time() - t}s)")
 
-test = numpy.array(test)
-result = p.predict(test[0:2])
-print(result if d == None else next(k for k, v in d.items() if v == result))
+if do_test:
+    test = numpy.array(test)
+    result = p.predict(test[0:2])
+    print(result if d == None else next(k for k, v in d.items() if v == result))
 
 frames = p.frames_to_display
 animation.display(frames, data, millisecondes=1)
